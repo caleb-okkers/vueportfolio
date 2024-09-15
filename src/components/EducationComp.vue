838d588188b2f32ec31bfc900ce35e82c44f694d
caleb-okkers <okkerscaleb.com>
2024-09-15 14:48:03 +0200

    update

--- src/components/EducationComp.vue
+++ src/components/EducationComp.vue
@@ -55,8 +55,8 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 
 .edu-heading {
-  color: #772aff;
-  text-shadow: 2px 2px 4px #000, 5px 5px 70px #2196F3;
+  color: #89ff00;
+  text-shadow: 2px 2px 4px #000, 5px 5px 70px #00bcd4;
 }
 
 .timeline {
@@ -163,7 +163,7 @@ export default {
   border: 3px solid;
   border-image-slice: 1;
   border-width: 3px;
-  border-image-source: linear-gradient(235deg, #772aff, #b6b2b2, #772aff);
+  border-image-source: linear-gradient(235deg, #89ff00, #b6b2b2, #89ff00);
   width: 400px;
   box-shadow: 0 0 10px #2196F3;
 }
@@ -211,7 +211,7 @@ export default {
   z-index: 1;
   border-top: 15px solid transparent;
   border-bottom: 15px solid transparent;
-  border-left: 15px solid #772aff;
+  border-left: 15px solid #89ff00;
   right: -15px;
 }
 
@@ -223,7 +223,7 @@ export default {
   z-index: 1;
   border-top: 15px solid transparent;
   border-bottom: 15px solid transparent;
-  border-right: 15px solid #772aff;
+  border-right: 15px solid #89ff00;
   left: -15px;
 }
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <footer class="container-fluid">
         <div class="row">
             <p class="lead">Caleb Okkers &copy; <span class="shadow">{{ cYear }}</span></p>
@@ -17,4 +17,81 @@ export default {
 </script>
 <style scoped>
     
+</style> -->
+
+<template>
+  <footer class="container-fluid">
+    <div class="row text-center py-4">
+      <div class="col-md-4">
+        <p class="mb-0">Athlone, Cape Town</p>
+      </div>
+      <div class="col-md-4">
+        <p class="mb-0">Email: okkerscaleb@gmail.com</p>
+      </div>
+      <div class="col-md-4">
+        <p class="mb-0">Phone: 078 344 9656</p>
+      </div>
+    </div>
+    <div class="row pt-3">
+      <p class="lead text-center w-100">
+        Caleb Okkers &copy; <span>{{ cYear }}</span>
+      </p>
+    </div>
+  </footer>
+</template>
+  
+  <script>
+export default {
+  name: "FooterComp",
+  data() {
+    return {
+      cYear: new Date().getUTCFullYear(),
+    };
+  },
+};
+</script>
+  
+  <style scoped>
+footer {
+  /* color: var(--primary-light);
+    background: var(--primary-dark); */
+  /* font-family: "Bona Nova SC", serif;
+    font-weight: 500;
+    font-style: normal;
+    font-size: 1rem; */
+  bottom: 0;
+  z-index: 1;
+}
+
+footer p,
+footer a {
+  /* color: var(--primary-light); */
+  color: #fff;
+  text-decoration: none;
+}
+
+footer ul {
+  list-style: none;
+  padding: 0;
+}
+
+footer ul li a:hover {
+  /* color: var(--primary); */
+  color: #fff;
+}
+
+footer .row {
+  margin: 0;
+}
+
+footer .lead {
+  padding-top: 1rem;
+}
+
+svg {
+  width: 2rem;
+  padding: 0.3rem;
+  margin-left: 1.5rem;
+  margin-right: 1.5rem;
+}
 </style>
